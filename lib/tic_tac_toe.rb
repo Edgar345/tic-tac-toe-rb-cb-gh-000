6771cbb51e839a2c9510128db9781a181bf286cd
Edgar V <evenegas9@ucmerced.edu>
2019-09-01 07:29:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,15 @@
 # Helper Method
 def play board
   puts "play method"
+  # until the game is over
+  #   take turns
+  # end
+  #
+  # if the game was won
+  #   congratulate the winner
+  # else if the game was a draw
+  #   tell the players it has been a draw
+  # end
 end
 
 def turn_count board
